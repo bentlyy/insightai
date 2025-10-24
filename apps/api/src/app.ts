0a21cca8286d181655f96ff3754f5cce466b6ca3
bentlyy <garayaa0606@gmail.com>
2025-10-24 10:59:29 -0300

    Inicializacion del proyecto: api, ml, web

---
+++ apps/api/src/app.ts
@@ -0,0 +1,14 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (_, res) => {
+  res.json({ message: "InsightAI API is running ✅" });
+});
+
+export default app;
